f031ac022d963d966f31d5d65ff3538d03d1d57a
Jason Walker <jason@onetrue.name>
2022-03-15 15:37:41 -0700

    bash boilerplate

---
+++ bash/boilerplate.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Boilerplate for a simple bash script
+# Inspired by: https://github.com/xwmx/bash-boilerplate/blob/master/bash-simple
+# TODO: Replace with appropriate comments for the script
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# This program's basename.
+_ME="$(basename "${0}")"
+
+_main() {
+}
+
+_main "$@"
